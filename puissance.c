b646e89568987680c304aa289b87f3112444fcc0
Emery Kouassi Assogba <assogba.emery@gmail.com>
2021-06-29 10:38:28 +0100

    remove main part

--- puissance.c
+++ puissance.c
@@ -2,7 +2,6 @@
 #include <limits.h>
 #include <stdlib.h>
 
-
 int power(int p, int n){
    int i;
    x = 1;
@@ -19,26 +18,3 @@ int power(int p, int n){
    return(x);
 }
 
-int main(int argc, char*argv[]){
-   int i;
-   i = random()%12;
-   while (i<2)
-      i= rand()%12;
-   
-   printf("power %d^%d = %d\n", i,i, power(i,i));
-   
-   i = random()%12;
-   while (i<2)
-      i= rand()%12;
-   
-   printf("power %d^%d = %d\n", i,i, power(0,1));
-   
-   printf("power %d^%d = %d\n", i,i, power(1,0));
-   
-   printf("power %d^%d = %d\n", i,i, power(0,0));
-   
-   printf("powerbis %d^%d = %d\n", i,i, power(INT_MAX,1));
-   
-   printf("%d\n", INT_MAX);
-   return 0;
-}
